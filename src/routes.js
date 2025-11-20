22a018e5198fea6949d0123a08d5c682f63440bc
Javi <francisco.javier.millan.mateos@alumnojoyfe.iepgroup.es>
2025-11-20 21:39:29 +0100

    prueba 6

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from './assets/pg/HomePage.vue';
 import pokedex from './components/pokedex.vue';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(), // 👈 Cambiado
     routes: [
         {
             path: '/',
